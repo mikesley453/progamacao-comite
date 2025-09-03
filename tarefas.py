cf71ec59f03f7240a37689d18a10f53b0d2e941e
mikesley453 <mikesleydias@gmail.com>
2025-09-02 21:45:52 -0300

    criação do programa de tarefas

--- tarefas.py
+++ tarefas.py
@@ -1,11 +1,25 @@
+# Calculando o dobro do numero 
 numero_inteiro= int(input("informe um numero inteiro ->"))
 print("o dobro deste numero é ->", numero_inteiro * 2 )
+
+# Calculando a area do quadrado
+lado = float(input(" informe o valor do lado do quadrado ->"))
+
+# Calculando a area do quadrado 
+print("a area do quadrado -> ", lado * lado )
 a_quadrado= int(input(" informe a area do quadrado ->")) 
 print(" o valor da area do quadrado é ->", a_quadrado * a_quadrado)
+
+# Calculando a idade em anos, meses e anos 
 idade= int(input(" informe a sua idade ->")) 
 meses = idade * 12 
+dias = meses * 365
 print(" sua idade em meses é ->", meses )
 print("sua idade em aproximadamente 365 dias vai ser ->", meses * 12)
-numero = int(input("informe um numero ->"))
 
 
+numero1 = float(input("informe o primeiro numero ->"))
+numero2 = float(input("informe o segundo numero ->"))
+numero3 = float(input("informe o terceiro numero "))
+
+print(" A media é -> ", (numero1 + numero2 + numero3) /3)
