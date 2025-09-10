eddfde492763a1f332c6c1df51168ab6faaee38d
mikesley453 <mikesleydias@gmail.com>
2025-09-10 20:38:53 -0300

    caculadora simples

---
+++ condicionais/calculadora_simples.py
@@ -0,0 +1,21 @@
+numero_1 = int(input(" informe um numero ->"))
+numero_2 = int(input(" informe mais um numero ->"))
+operacoes = input(" diga uma operação ->")
+
+if operacoes == "+" : 
+    print(" resultado da sua conta é ->", numero_1 + numero_2 )
+
+elif operacoes == "-":
+    print(" resulado da sua conta é ->", numero_1 - numero_2)
+
+elif operacoes == "*":
+    print(" rsultado da sua conta é ->")
+
+elif operacoes == "/": 
+    print(" resultado da conta é ->")
+else:
+    print(" operacao invalida")
+
+
+
+
--- condicionais/maior_100.py
+++ condicionais/maior_100.py
@@ -1,5 +1,5 @@
 valor_1 = float(input(" informe o primeiro valor ->"))
-valor_2 = float(input(" infrome o segundo valor ->"))
+valor_2 = float(input(" informe o segundo valor ->"))
 
 if valor_1 + valor_1 > 100:
     print(" a soma dos valores é maior que 100")
